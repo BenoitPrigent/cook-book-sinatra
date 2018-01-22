90669a2a7c7dcb0b469693107c43cb08b4ede9fd
Benoit Prigent <benoit.prigent@grenoble-em.com>
2018-01-22 17:32:39 +0100

    coucou

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+
 get '/' do
-  'Hello world!'
+  erb :index
+
+end
+
+
+get '/about' do
+  erb :about
 end
